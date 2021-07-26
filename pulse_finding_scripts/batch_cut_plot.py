a6fb2e31108e65f1e773ddc595a7120049660a11
rhingorani10 <rhingorani@lbl.gov>
2021-07-26 15:29:01 -0700

    add option to skip processed folder

--- pulse_finding_scripts/batch_cut_plot.py
+++ pulse_finding_scripts/batch_cut_plot.py
@@ -1,5 +1,5 @@
 from cut_plot import make_plots
-
+from pathlib import Path
 from glob import glob
 
 #process all data sets but the calibration data under this main folder
@@ -12,6 +12,17 @@ list_dir = glob(path_text+"*/")
 
 for i in reversed(range(len(list_dir))):
     if ("spe" in list_dir[i]) or ("dark" in list_dir[i]): list_dir.pop(i)
+
+print("\n Process all the data(1), or only those ones not processed yet(2)?\n")
+mode = input()
+if mode == "2":
+    for i in reversed(range(len(list_dir))):
+    	rq_file = Path(list_dir[i]+"rq.npz")
+    	if not rq_file.exists(): list_dir.pop(i)
+    	fig_file = Path(list_dir[i]+"DriftTime_AS.png")
+    	if fig_file.exists(): list_dir.pop(i)
+else:
+    pass
 
 print("\n Data to process:\n")
 print('\n'.join(list_dir))
