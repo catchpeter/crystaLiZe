1da3c9cdc878e054be81133ab1af4891c2d7e6e9
maquechen <maquechen@gmail.com>
2021-07-26 20:59:30 -0700

    change name

---
+++ pulse_finding_scripts/batch_rq_generate.py
@@ -0,0 +1,35 @@
+from rq_generate import make_rq
+from glob import glob
+from pathlib import Path
+
+#process all data sets but the calibration data under this main folder
+
+with open("batch_path.txt", 'r') as path:
+    path_text = path.read()
+
+list_dir = glob(path_text+"*/")
+
+
+for i in reversed(range(len(list_dir))):
+    if ("spe" in list_dir[i]) or ("dark" in list_dir[i]): list_dir.pop(i)
+
+print("\n Process all the data(1), or only those ones not processed yet(2)?\n")
+mode = input()
+if mode == "2":
+    for i in reversed(range(len(list_dir))):
+        rq_file = Path(list_dir[i]+"rq.npz")
+        if rq_file.exists(): list_dir.pop(i)
+else:
+    pass
+
+print("\n Data to process:\n")
+print('\n'.join(list_dir))
+print("\n Check the data list above, if not correct, press q then Enter. Otherwise, press any other key to start\n")
+
+flag = input()
+if flag == "q": exit()
+
+for data_dir in list_dir:
+    print("Now start to process:"+data_dir)
+
+    make_rq(data_dir)
