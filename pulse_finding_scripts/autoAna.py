191f8914ae9db258cc4134db205632afdbc4d586
Crystalize Analysis <xaber@theo.localdomain>
2022-09-12 16:16:40 -0700

    added autoAna for the desktop

---
+++ pulse_finding_scripts/autoAna.py
@@ -0,0 +1,55 @@
+import glob
+import os
+import time
+
+from compression import compression
+from rq_generate_32ch import make_rq
+from cut_plot import make_plots
+
+
+
+
+def autoAna():
+
+    data_base_dir = "/media/xaber/extradrive1/crystalize_data/"
+    all_dir = glob.glob(data_base_dir + "*/*/*/")
+
+    # Not particularly efficient at finding data. Balanced by our beefy desktop
+    for dir in all_dir:
+
+        # Check if data has finished transfering
+        if not os.path.exists(dir + "/readyToAnalyze"):
+            continue
+
+        # Check to compress
+        if not os.path.exists(dir + "compressed_data/compressed_0.npy"):
+            try:
+                compression(dir)
+            except:
+                print("uh oh compression didn't work")
+
+        # Check to generate rq's and plots
+        if not os.path.exists(dir + "rq.npz"):
+            try:
+                make_rq(dir)
+                make_plots(dir)
+            except:
+                print("uh oh rq didn't work")
+
+    return 
+
+
+def main():
+
+    while True: # lmao
+
+        autoAna()
+
+        # Checks every minute (roughly)
+        time.sleep(60)
+
+    return 
+
+
+if __name__ == "__main__":
+    main()
