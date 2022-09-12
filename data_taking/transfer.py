32c3a22c375b47eab1c5552d4586d091ec73d866
Crystalize Analysis <xaber@theo.localdomain>
2022-09-12 16:30:42 -0700

    added transfer script for daq laptop

---
+++ data_taking/transfer.py
@@ -0,0 +1,42 @@
+import glob
+import os 
+import time
+
+
+
+def transfer():
+
+    data_base_dir = 
+    all_dir = glob.glob(data_base_dir + "*/*/*/")
+
+    # Check if data aquisition is finished
+
+
+
+    # Transfer data
+
+
+
+    # Transfer analysis flag
+
+
+    return
+
+
+
+
+def main():
+
+    while True: # hehe hoho haha
+
+        transfer()
+
+        # Checks every minute (roughly)
+        time.sleep(60)
+
+
+    return 
+
+
+if __name__ == "__main__":
+    main()
