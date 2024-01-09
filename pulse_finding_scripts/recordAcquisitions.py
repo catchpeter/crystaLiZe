896b368b41015e6390b8b6e6aa3c8b85d32a757c
rmg98 <rgibbons@berkeley.edu>
2024-01-09 11:55:47 -0800

    New acquisition spreadsheet gen

---
+++ pulse_finding_scripts/recordAcquisitions.py
@@ -0,0 +1,97 @@
+import numpy as np
+import time
+import glob
+import datetime as dt
+import os
+
+
+def recordAcquisitions(start_time):
+
+    # Tracking spreadsheet file name
+    run_name = "test" #"24A"
+    csv_file_name = f"/home/xaber/crystalize/Analysis/acquisitions/run_{run_name}.xlsx"
+
+    # If it exists, get the final line, which is the start time to start recording new entries
+    csv_exists = os.path.isfile(csv_file_name)
+    if csv_exists:
+        with open(csv_file_name, "r") as f:
+            last_line = f.readlines()[-1]
+            last_dt = last_line[:15]
+            start_time = dt.datetime(int(last_dt[:4]), int(last_dt[4:6]), int(last_dt[6:8]), int(last_dt[9:11]), int(last_dt[11:13]), int(last_dt[13:15]))
+    
+    # Where to look for data
+    data_dir_base = "/media/xaber/outSSD1/crystalize_data/"
+    long_list = sorted(glob.glob(data_dir_base+"*/*/*/"))
+
+    # Loop over data dirs
+    for i, data_dir in enumerate(long_list):
+
+        # Again, does the tracking spreadsheet exist?
+        csv_exists = os.path.isfile(csv_file_name)
+
+        # Get the datetime
+        dt_stamp = data_dir[-16:-1]
+        dt_dt = dt.datetime(int(dt_stamp[:4]), int(dt_stamp[4:6]), int(dt_stamp[6:8]), int(dt_stamp[9:11]), int(dt_stamp[11:13]), int(dt_stamp[13:15]))
+
+        # Should we record it?
+        if (dt_dt - start_time).total_seconds() > 1:
+
+            # Open the conditions file
+            conds = np.loadtxt(data_dir+"conditions.csv", delimiter=",", dtype=str)
+            fields = conds[0].tolist()
+            values = conds[1].tolist()
+            
+            # Add the location on theo
+            fields.append("Location")
+            values.append(data_dir)
+
+            # If it doesn't exist, record the fields as well
+            if not csv_exists:
+                with open(csv_file_name, "w") as f:
+                    np.savetxt(f, [fields,values], delimiter=",", fmt="%s")
+            # Otherwise, record just the values
+            else:
+                with open(csv_file_name, "a") as f:
+                    np.savetxt(f, [values], delimiter=",", fmt="%s")
+
+
+    # Send it to the cloud
+    command = "rclone -v copy " + csv_file_name +" gdrive:crystallize/data/tracking/acquisitions/"
+    csv_exists = os.path.isfile(csv_file_name)
+    if csv_exists:
+        try:
+            os.system(command)
+        except:
+            print("Error in upload, probably reached the limit")
+
+
+    
+    return
+
+
+
+
+
+
+def main():
+
+    start_time = dt.datetime(2024,1,1,0,0,0)
+
+    while True:
+
+        start_time = recordAcquisitions(start_time)
+
+        time.sleep(5*60)
+
+
+    return
+
+
+   
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
