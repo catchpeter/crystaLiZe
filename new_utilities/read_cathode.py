779136087e13200c1f39f24bab756ca11a3e75b6
rmg98 <rgibbons@berkeley.edu>
2024-03-06 07:35:42 -0800

    minor change to read HV scripts

--- new_utilities/read_cathode.py
+++ new_utilities/read_cathode.py
@@ -6,9 +6,10 @@ def main():
 
     usb_n = sf.get_usb_n(name="cathode")
     value = sf.read_hv(usb_n)
-    print(f"Cathode at -{value} V")
-    
-    return 
+    #print(f"Cathode at -{value} V")
+    print(value)
+
+    return value 
 
 if __name__ == "__main__":
     main()
--- new_utilities/read_gate.py
+++ new_utilities/read_gate.py
@@ -6,9 +6,10 @@ def main():
 
     usb_n = sf.get_usb_n(name="gate")
     value = sf.read_hv(usb_n)
-    print(f"Gate at -{value} V")
-    
-    return 
+    #print(f"Gate at -{value} V")
+    print(value)
+
+    return value 
 
 if __name__ == "__main__":
     main()
