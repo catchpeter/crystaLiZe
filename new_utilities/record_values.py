5d0377ba7bdbe633d3ae8452f1b40ba195c7ed5a
rmg98 <rgibbons@berkeley.edu>
2024-03-06 13:01:29 -0800

    simple stable added, bug fix to record values

--- new_utilities/record_values.py
+++ new_utilities/record_values.py
@@ -107,25 +107,28 @@ def record_values():
             ftemp.write("%s,%s,%f,%3.3f,%3.3f,%3.3f,%3.3f,%3.3f,%3.3f,%3.3f\n" % save_list)
 
         # Read heaters settings
-        with open("/home/xaber/solid_xenon_tpc/new_utilities/heaters_setting.txt") as f:
-            f.readline()
-            power_line = f.readline()
-            powers = power_line.split(",")
-        top_heater_power = float(powers[0])
-        bottom_heater_power = float(powers[1])
+        try:
+            with open("/home/xaber/solid_xenon_tpc/new_utilities/heaters_setting.txt") as f:
+                f.readline()
+                power_line = f.readline()
+                powers = power_line.split(",")
+            top_heater_power = float(powers[0])
+            bottom_heater_power = float(powers[1])
 
-        # Change heaters settings
-        if abs(top_heater_power-current_top_power)>0.005:
-            print("the current top heater power is {:.2f}W, will change to {:.2f}W".format(current_top_power, top_heater_power))
-            top_heater_v = np.sqrt(25.*top_heater_power)
-            (v_top_now, i_top_now) = sf.change_heater(heater_usb_n, 1, top_heater_v)
-            current_top_power = top_heater_power
-        if abs(bottom_heater_power-current_bottom_power)>0.005:
-            print("the current bottome heater power is {:.2f}W, will change to {:.2f}W".format(current_bottom_power, bottom_heater_power))
-            bot_heater_v = np.sqrt(25.*bottom_heater_power)
-            (v_bottom_now, i_bottom_now) = sf.change_heater(heater_usb_n, 2, bot_heater_v)
-            current_bottom_power= bottom_heater_power
- 
+            # Change heaters settings
+            if abs(top_heater_power-current_top_power)>0.005:
+                print("the current top heater power is {:.2f}W, will change to {:.2f}W".format(current_top_power, top_heater_power))
+                top_heater_v = np.sqrt(25.*top_heater_power)
+                (v_top_now, i_top_now) = sf.change_heater(heater_usb_n, 1, top_heater_v)
+                current_top_power = top_heater_power
+            if abs(bottom_heater_power-current_bottom_power)>0.005:
+                print("the current bottome heater power is {:.2f}W, will change to {:.2f}W".format(current_bottom_power, bottom_heater_power))
+                bot_heater_v = np.sqrt(25.*bottom_heater_power)
+                (v_bottom_now, i_bottom_now) = sf.change_heater(heater_usb_n, 2, bot_heater_v)
+                current_bottom_power= bottom_heater_power
+        except:
+            print("error in reading/writing")
+
         # Sleep until next iteration
         time.sleep(read_period_sec)
 
---
+++ new_utilities/simple_stable_new.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# 2023-12-06 pfs/rmg 
+# 2023-12-08 pfs add new cases since pressure is rising beyond where we want to keep it
+# 2023-12-10 pfs further tuning on acceptable pressure range
+# 2024-03-06 pfs/rmg updating for new MeasurementComputing ADC and new slowcontrol box (xena)
+
+import sys
+import os
+import time
+import subprocess
+import serial
+import numpy as np
+import csv
+
+
+def wriiit(Wt,Wb):
+	f=open(filename2,"w")
+	f.write("top, bot\n%1.2f, %1.2f\n" % (Wt,Wb))
+	f.close()
+
+filename1='/home/xaber/ttlogs/current.csv'
+filename2='/home/xaber/solid_xenon_tpc/new_utilities/heaters_setting.txt'
+
+# read heaters_setting.txt ONCE at the start to get the baseline settings
+with open(filename2) as file:
+	csvFile = csv.reader(file)
+	for lines in csvFile: # ignores the first line
+		tmp=lines[0]
+	Wt = float(lines[0]) # latest read of top heater power (Watts)
+	Wb = float(lines[1]) # latest read of bot heater power (Watts)
+
+#for i in range(1,2):
+while True:
+	try:
+		with open(filename1) as file:
+			csvFile = csv.reader(file)
+			for lines in csvFile:
+				PB = float(lines[7]) # latest read of pressure (Bar)
+
+		print ("pressure now %1.3f and heater powers: %1.3f W (top) %1.3f W (bot)" % (PB,Wt,Wb) )
+
+		# 0.818 Bar triple point	
+		# set point p
+		spp = 0.84 # crystal triple phase setting
+		spp = 1.17 # liquid/vapor (note: approx stable with 2.15,2.15)
+		if ( (PB<=(spp+0.01)) and (PB>=(spp-0.01)) ): # do nothing
+			print("P=%1.3f Bar - in range!" % PB)
+			wriiit(Wt,Wb)							
+
+		if (PB>(spp+0.01)):
+			print("P=%1.3f Bar - above range!" % PB)
+			wriiit(Wt,Wb*0.9)							
+			if (PB>(spp+0.02)):
+				wriiit(Wt,Wb*0.8)
+				if (PB>(spp+0.03)):
+					wriiit(Wt,Wb*0.7)
+					if (PB>(spp+0.04)):
+						wriiit(Wt,Wb*0.6)
+						if (PB>(spp+0.05)):
+							wriiit(Wt,Wb*0.5)
+
+		if (PB<(spp-0.01)):
+			print("P=%1.3f Bar - below range!" % PB)
+			wriiit(Wt,Wb*1.1)
+			if (PB<(spp-0.02)):
+				wriiit(Wt,Wb*1.2)
+				if (PB<(spp-0.03)):
+					wriiit(Wt,Wb*1.3)
+					if (PB<(spp-0.04)):
+						wriiit(Wt,Wb*1.4)
+						if (PB<(spp-0.05)):
+							wriiit(Wt,Wb*1.5)
+		time.sleep(5)
+		
+	except KeyboardInterrupt: # does not work...
+		stored_exception=sys.exc_info()
+	
+
+
+
+
+
+
+
