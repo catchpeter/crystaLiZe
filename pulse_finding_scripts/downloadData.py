f43a21c0fe87760753329f17bfcc74c9e52fcd29
rmg98 <rgibbons@berkeley.edu>
2024-03-19 11:30:49 -0700

    clean up

--- pulse_finding_scripts/downloadData.py
+++ pulse_finding_scripts/downloadData.py
@@ -1,13 +1,14 @@
+"""
+Simple script to grab data from google drive
+"""
+
 import os
 
+# Data location on google drive
+base_command = "rclone -v copy gdrive:crystallize/data/data-202210/20221004/20221004-1538_2DR_20mVtrig_15us_3202.0C_3001.0G_500A_54SiPM_1.22bar_-97.36ICVbot_2fold_CoOCVtop_20min/"
 
-base_command = "rclone -v copy gdrive:crystallize/data/data-202207/20220715/20220715-1214_2DR_6mVtrig_15us_3201.0C_3001.0G_500A_54SiPM_0.72bar_-115.48ICVbot_2fold_CoOCVtop_120min/" 
-#data_dir = "/media/xaber/extradrive4/crystalize_data/data-202211/20221105/20221105-0732_2DR_10mVtrig_30us_5202.0C_5002.0G_500A_54SiPM_1.36bar_-102.19ICVbot_2fold_degraded_afterFill_180min/"
-data_dir = "/media/xaber/extradrive4/crystalize_data/data-202207/20220715/20220715-1214_2DR_6mVtrig_15us_3201.0C_3001.0G_500A_54SiPM_0.72bar_-115.48ICVbot_2fold_CoOCVtop_120min/"
-n_channels = [16,8,8]
-for bd in range(3):
-    for ch in range(n_channels[bd]):
-        command = base_command+"waveforms_"+str(bd)+"_"+str(ch)+".dat "+ data_dir
+# Local location
+data_dir = "/media/xaber/G-Drive2/crystalize_data/data-202210/20221004/20221004-1538_2DR_20mVtrig_15us_3202.0C_3001.0G_500A_54SiPM_1.22bar_-97.36ICVbot_2fold_CoOCVtop_20min/"
 
-        os.system(command)
-
+command = base_command + " " + data_dir
+os.system(command)
